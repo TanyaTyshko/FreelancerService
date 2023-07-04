69ff4a514dd3f47e77c5ae9a6232ef14f684e522
Tanya <Tanja_33@mail.ru>
2023-07-04 18:33:33 +0700

    New

--- src/test/java/ru/netology/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
- class FreelancerServiceTest {
+class FreelancerServiceTest {
     @Test
     public void testCalculate1() {
         FreelancerService service = new FreelancerService();
